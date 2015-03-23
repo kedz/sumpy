f0e16368f0c3c80c5cf5705353389060acb7e008
kedz <kedzie@cs.columbia.edu>
2015-03-23 18:10:42 -0400

    Fixed minor bug

--- duc_testbed.py
+++ duc_testbed.py
@@ -23,17 +23,17 @@ def load_docset(docset_path):
     for fname in os.listdir(docset_path):
         if docset_id in fname:
             model_paths = [os.path.join(docset_path, fname, length)
-                           for length in [u"50", u"100", u"200", u"400"]]
+                           for length in [u"200", u"400"]]
             model_sums = sumpy.io.load_duc_abstractive_summaries(model_paths)
             models.extend(model_sums)
     return docset_id, docs, models
 
 
 def generate_summaries(systems, docsets):
-    rouge = sumpy.rouge.ROUGE(max_ngrams=2, show_per_model_results=False)
+    rouge = sumpy.rouge.ROUGE(max_ngrams=2, limit=100, limit_type=u"word")
     results = []
     for docset_id in docsets.keys():
-        print docset_id
+        #print docset_id
         docs = docsets[docset_id][u"docs"]
         models = docsets[docset_id][u"models"]
         sys_sums = [(system_name, unicode(sum_func(docs)))
@@ -47,8 +47,11 @@ def main(duc_dir):
     docsets = load_docsets(duc_dir)
     
     lede = lambda x: sumpy.lede(x)
-    centroid = lambda x: sumpy.centroid
-    print generate_summaries([("lede", lede), ("centroid", centroid)], docsets)
+    centroid = lambda x: sumpy.centroid(x)
+    lexrank = lambda x: sumpy.lexrank(x)
+    systems = [(u"lede", lede), (u"centroid", centroid),
+               (u"lexrank", lexrank)]
+    print generate_summaries(systems, docsets)
 
 if __name__ == u"__main__":
     parser = argparse.ArgumentParser()
