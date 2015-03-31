9d0d68329efd8d63b9079aaeef3b0fa3e3e6dece
Yanrong Wo <yw2513@columbia.edu>
2015-03-31 02:43:54 -0400

    Debugged DEMS in rankers

--- sumpy/rankers.py
+++ sumpy/rankers.py
@@ -2,36 +2,38 @@ import numpy as np
 from itertools import combinations
 from scipy.sparse import csr_matrix
 from sklearn.metrics.pairwise import cosine_similarity
+import pkg_resources
+import os
 
 class DEMSRankerMixin(object):
     def demsrank(self, input_df, lead_word_weight=1, verb_spec_weight=1):
         lead_path = pkg_resources.resource_filename("sumpy", 
                         os.path.join("data", "lead_words.txt"))
         self._leadwords = []
-        with gzip.open(lead_path, u"r") as f:
+        with open(lead_path, u"r") as f:
             text = f.readlines()
             for line in text:
                 line_split = line.split()
-                self.lead_words.append(line_split[0])
+                self._leadwords.append(line_split[0])
         verb_path = pkg_resources.resource_filename("sumpy", 
                         os.path.join("data", "verb_specificity.txt"))
         self._verbspec = {}
-        with gzip.open(verb_path, u"r") as f:
+        with open(verb_path, u"r") as f:
             text = f.readlines()
             for line in text:
                 line_split = line.split()
-                self.verbspec[line_split[0]] = line_split[1]
+                self._verbspec[line_split[0]] = line_split[1]
 
-        lead_score = np.zeros(input_df.size)
-        verb_score = np.zeros(input_df.size)
-        for i in range(0, input_df.size):
-            lead_score[i] = _get_lead_values(input_df['text'][i])
-            verb_score[i] = _get_verb_specificity(input_df['text'][i])
+        lead_score = np.zeros(len(input_df.index))
+        verb_score = np.zeros(len(input_df.index))
+        for i in range(0, len(input_df.index)):
+            lead_score[i] = self._get_lead_values(input_df['text'][i])
+            verb_score[i] = self._get_verb_specificity(input_df['text'][i])
         lead_score = lead_score / np.amax(lead_score)
         verb_score = verb_score / np.amax(verb_score)
         input_df[u"rank:demsrank"] = lead_score + verb_score
 
-    def _get_lead_values(sent):
+    def _get_lead_values(self, sent):
         word_count = 0
         lead_word_count = 0
         for token in sent:
@@ -40,7 +42,7 @@ class DEMSRankerMixin(object):
                 lead_word_count = lead_word_count + 1
         return float(lead_word_count) / word_count
 
-    def _get_verb_specificity(sent):
+    def _get_verb_specificity(self, sent):
         max_val = 0
         for token in sent:
             if token in self._verbspec.keys() and float(self._verbspec[token]) > max_val:
