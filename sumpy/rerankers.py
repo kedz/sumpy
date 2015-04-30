33482793b398b8ade912261fa4185cd3f6598ea2
Yanrong Wo <yw2513@columbia.edu>
2015-04-30 05:40:43 -0400

    Modified rerankers to work with fLAI for submod descent alg

--- sumpy/rerankers.py
+++ sumpy/rerankers.py
@@ -6,20 +6,20 @@ class fmmr(CorpusTfidfMixin):
     def __init__(self, input_df):
         tfidfer = self.build_tfidf_vectorizer()
         self.tfidf_mat = tfidfer(input_df[u"words"].tolist())
+        self.input_df = input_df
 
-    def get_fmmr_value(self, in_summary, in_indicies, not_summary,
-        not_indicies, l=4):
+    def rank(self, in_indices, not_indices, l=4):
         sum_sim = 0
         sum_red= 0
-        for i in range(0, len(in_indicies)):
-            for j in range(i+1, len(in_indicies)):
-                first_index = in_indicies[i]
-                second_index = in_indicies[j]
+        for i in range(0, len(in_indices)):
+            for j in range(i+1, len(in_indices)):
+                first_index = in_indices[i]
+                second_index = in_indices[j]
                 sum_red += self.tfidf_mat[first_index][second_index]
-        for i in range(0, len(in_indicies)):
-            for j in range(i+1, len(not_indicies)):
-                first_index = in_indicies[i]
-                second_index = not_indicies[j]
+        for i in range(0, len(in_indices)):
+            for j in range(i+1, len(not_indices)):
+                first_index = in_indices[i]
+                second_index = not_indices[j]
                 sum_sim += self.tfidf_mat[first_index, second_index]
         return sum_sim - l*sum_red
 
@@ -28,10 +28,9 @@ class GreedyReranker(fmmr):
         #Set default ranker to fmmr if not defined
         if not ranker:
             func = fmmr(input_df)
-            ranker= func.get_fmmr_value
+            ranker= func.rank
         #initialize values
-        summary = None
-        summary_indicies = []
+        summary_indices = []
         sent_remaining = range(0, len(input_df['text']))
         length = 0
         #Keep adding sentences while there are more sentences and underbudget
@@ -40,26 +39,18 @@ class GreedyReranker(fmmr):
             best_index = None
             for i in range(0, len(sent_remaining)):
                 index = sent_remaining[i]
-                #Get the sentences/indicies not in the summary
-                remaining = input_df.copy(deep=True).drop(summary_indicies)
-                remaining_indicies = range(0, len(input_df.index))
-                for summary_index in summary_indicies:
-                    remaining_indicies.remove(summary_index)
+                #Get the sentences/indices not in the summary
+                remaining_indices = range(0, len(input_df.index))
+                for summary_index in summary_indices:
+                    remaining_indices.remove(summary_index)
                 #Calculate current value of summary
-                old_value = ranker(summary, summary_indicies, remaining, 
-                    remaining_indicies)
-                #Move the current index from the remaining sent/indicies
-                #to the summary sent/indicies
-                remaining.drop(index)
-                remaining_indicies.remove(index)
-                copy_summary = None
-                if summary:
-                    copy_summary = summary.copy(deep=True).concat(
-                        input_df.loc[[index]])
-                else:
-                    copy_summary = input_df.loc[[index]]
-                new_value = ranker(copy_summary, summary_indicies + [index], 
-                    remaining, remaining_indicies)
+                old_value = ranker(summary_indices, 
+                    remaining_indices)
+                #Move the current index from the remaining sent/indices
+                #to the summary sent/indices
+                remaining_indices.remove(index)
+                new_value = ranker(summary_indices + [index], 
+                    remaining_indices)
                 #Calculate the gain and update if greater than current max
                 gain = (new_value - old_value) / \
                     (len(input_df['text'][index])**r)
@@ -69,36 +60,30 @@ class GreedyReranker(fmmr):
             #Make sure max is pos and length under budget
             #If so, add it to the summary
             if max_gain > 0 and length + len(best_sent) <= budget:
-                summary.concat(input_df.loc[[best_index]])
-                summary_indicies.append(best_index)
+                summary_indices.append(best_index)
                 length = length + len(best_sent)
             sent_remaining.remove(best_index)
         #Find the max ranking value for one sentence
         max_sing_value = None
         max_sing_index = None
-        max_sing_sent = ""
         for i in range(0, len(input_df['text'])):
-            in_summary = input_df.loc[[i]]
-            in_indicies = [i]
-            out_summary = input_df.copy(deep=True).drop(i)
-            out_indicies = range(0, len(input_df.index))
-            out_indicies.remove(i)
-            current_value = ranker(in_summary, in_indicies, out_summary, out_indicies)
+            in_indices = [i]
+            out_indices = range(0, len(input_df.index))
+            out_indices.remove(i)
+            current_value = ranker(in_indices, out_indices)
             if current_value > max_sing_value:
                 max_sing_value = current_value
                 max_sing_index = i
-                max_sing_sent = input_df['text'][i]
         #Check if the max value for one sent is greater than the summary
-        remaining = input_df.copy(deep=True).drop(summary_indicies)
-        remaining_indicies = range(0, len(input_df.index))
-        for summary_index in summary_indicies:
-            remaining_indicies.remove(summary_index)
-        if max_sing_value > ranker(summary, summary_indicies, remaining, \
-            remaining_indicies):
-            summary =  max_sing_sent
-            summary_indicies = [max_sing_index]
+        remaining_indices = range(0, len(input_df.index))
+        for summary_index in summary_indices:
+            remaining_indices.remove(summary_index)
+        current_score = ranker(summary_indices, remaining_indices)
+        if max_sing_value > current_score:
+            summary_indices = [max_sing_index]
+        self.indices = summary_indices
         #Weight the ranks so they're between 0 and 1
         ranks = np.zeros(len(input_df.index))
-        for i,index in enumerate(reversed(summary_indicies)):
-            ranks[index] = i / len(summary_indicies)
+        for i,index in enumerate(reversed(summary_indices)):
+            ranks[index] = i / len(summary_indices)
         input_df['rank:reranker'] = ranks
