057156c05529ad1732c850c0a8a25401fb11f26f
Chris Kedzie <kedzie@cs.columbia.edu>
2015-06-03 16:52:30 -0400

    Added a pluggable submodular system for easy extending.

--- sumpy/system/__init__.py
+++ sumpy/system/__init__.py
@@ -2,8 +2,8 @@ from sumpy.system._base import AverageFeatureRankerBase
 from sumpy.system._baseline import (LedeSummarizer, CentroidSummarizer, 
         MMRSummarizer)
 from sumpy.system._graph import TextRankSummarizer, LexRankSummarizer
-from sumpy.system._submodular import SubmodularMMRSummarizer
+from sumpy.system._submodular import MonotoneSubmodularBasic, SubmodularMMRSummarizer
 
 __all__ = ["LedeSummarizer", "CentroidSummarizer", "MMRSummarizer", 
            "TextRankSummarizer", "LexRankSummarizer", 
-           "SubmodularMMRSummarizer, AverageFeatureRankerBase"]
+           "MonotoneSubmodularBasic", "SubmodularMMRSummarizer, AverageFeatureRankerBase"]
--- sumpy/system/_submodular.py
+++ sumpy/system/_submodular.py
@@ -1,5 +1,5 @@
 from sumpy.system._base import _SystemBase
-from sumpy.annotators import SubmodularMMRMixin
+from sumpy.annotators import MonotoneSubmodularMixin, SubmodularMMRMixin
 from sumpy.document import Summary
 
 class SubmodularMMRSummarizer(SubmodularMMRMixin, _SystemBase):
@@ -21,3 +21,21 @@ class SubmodularMMRSummarizer(SubmodularMMRMixin, _SystemBase):
         print output_df
         print output_df['sent text'].apply(len)
         return Summary(output_df)
+
+
+class MonotoneSubmodularBasic(MonotoneSubmodularMixin, _SystemBase):
+
+    def __init__(self, sentence_tokenizer=None, word_tokenizer=None,
+            k=5, f_of_A=None, verbose=False):
+        
+        self.sentence_tokenizer = sentence_tokenizer
+        self.word_tokenizer = word_tokenizer
+        self.k = k 
+        self.f_of_A = f_of_A
+        super(MonotoneSubmodularBasic, self).__init__(verbose=verbose)
+
+    def build_summary(self, input_df, ndarray_data):
+        output_df = input_df[input_df["f:monotone-submod"] == 1]
+        output_df = output_df.sort(["doc id", "sent id"], ascending=True)
+        return Summary(output_df)   
+
